10b5d45e6a33a8de5595e3d9c5a1968bc351d963
sarteleb <sarteleb@gmail.com>
2015-02-16 13:48:23 -0500

    Index 1 now displays all holidays for current month and year as well as for February 1993

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 # Gemfile
 source 'https://rubygems.org'
-ruby '2.2.0'
+ruby '2.0.0'
 
 gem 'sinatra', '~> 1.4.5'
+gem 'cat_api'
+gem 'holidapi'
+gem 'rack', '1.5.2'
--- app.rb
+++ app.rb
@@ -1,8 +1,38 @@
 # app.rb
 require 'sinatra'
+require 'cat_api'
+require 'holidapi'
+
 
 class MyWebApp < Sinatra::Base
   get '/' do
-    erb :"index#{ rand(3) + 1 }"
+    daysInMonth = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    time = Time.new
+    holidayArray = []
+    birthdayMonthArray = []
+    i = 1
+    while i <= daysInMonth[time.month]
+      holiday = HolidApi.get(country: 'us', year: time.year, month: time.month, day: i)
+      if  holiday[0] != nil
+        holidayArray.push(holiday)
+      end
+      i += 1
+    end
+    i = 1
+    while i <= daysInMonth[2]
+      holiday = HolidApi.get(country: 'us', year: 1993, month: 2, day: i)
+      if  holiday[0] != nil
+        birthdayMonthArray.push(holiday)
+      end
+      if i == 24
+        birthday = [{"name"=> "Brandon's Birthday (Yes it qualifies as a national holiday)", 'country'=> "US", 'date'=> "1993-02-24"}]
+        birthdayMonthArray.push(birthday)
+      end
+      i += 1
+    end
+    @holidays = holidayArray
+    @birthdayHolidays = birthdayMonthArray
+    @time = time
+    erb :index1
   end
 end
